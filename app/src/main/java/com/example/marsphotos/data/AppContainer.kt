ebd958699efee9dae39e57717e41acda5a59c9a5
David Armas Castresana <daarmas@unirioja.loc>
2023-03-09 18:42:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.marsphotos.data
+
+import com.example.marsphotos.network.MarsApiService
+import retrofit2.Retrofit
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+
+interface AppContainer {
+    val marsPhotosRepository: MarsPhotosRepository
+}
+
+class DefaultAppContainer : AppContainer {
+    private val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(BASE_URL)
+        .build()
+
+    private val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
+    override val marsPhotosRepository: MarsPhotosRepository by lazy {
+        NetworkMarsPhotosRepository(retrofitService)
+    }
+}
